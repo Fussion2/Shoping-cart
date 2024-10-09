802049f45b0796e52dde17bf32a44509012c38d7
Fussion2 <143224212+Fussion2@users.noreply.github.com>
2024-10-09 14:00:08 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Counter from "./components/Counter"; // Remove the extra space
+
+const App = () => {
+  return (
+    <div>
+      <Counter /> {/* Rendering the Counter component */}
+    </div>
+  );
+};
+
+export default App;
+
+
